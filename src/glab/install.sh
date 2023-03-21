0c21ed25a874ecb91cf52b75f33bc62b663c7ed5
Víctor Morilla <vmorilla@gmail.com>
2023-03-21 16:30:26 +0000

    Adds debian dependencies for glab installation

--- src/glab/install.sh
+++ src/glab/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
 
-RUN apt-get update \
-    && apt-get -y install gitlab-cli \
-    && apt-get autoremove -y \
+# Installs dependencies
+apt-get update \
+    && apt-get -y install curl gpg lsb-release
+
+# Adds prebuilt-mpr to apt sources
+curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' \
+    | gpg --dearmor > /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg \
+    && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" > /etc/apt/sources.list.d/prebuilt-mpr.list
+
+# Installs the cli
+apt-get update \
+    && apt-get -y install glab \
     && apt-get clean -y \
     && rm -rf /var/lib/apt/lists/*
